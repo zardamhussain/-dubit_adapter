d07003f55ccd25b3f22922f3787855ff72141a4d
zardamhussain <zardamhussain@gmail.com>
2024-11-29 05:39:58 +0000

    init

---
+++ lib/dubit_adapter.dart
@@ -0,0 +1,267 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:daily_flutter/daily_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class DubitEvent {
+  final String label;
+  final dynamic value;
+
+  DubitEvent(this.label, [this.value]);
+}
+
+enum DubitAudioDevice {
+  speakerphone,
+  wired,
+  earpiece,
+  bluetooth,
+}
+
+class Dubit {
+  final String apiKey;
+  final String? apiBaseUrl;
+  final _streamController = StreamController<DubitEvent>();
+
+  Stream<DubitEvent> get onEvent => _streamController.stream;
+
+  CallClient? _client;
+
+  Dubit(this.apiKey, [this.apiBaseUrl]);
+
+  Future<void> start({
+    Duration clientCreationTimeoutDuration = const Duration(seconds: 10),
+  }) async {
+    if (_client != null) {
+      throw Exception('Call already in progress');
+    }
+
+    print("🔄 ${DateTime.now()}: Dubit - Requesting Mic Permission...");
+    var microphoneStatus = await Permission.microphone.request();
+    if (microphoneStatus.isDenied) {
+      microphoneStatus = await Permission.microphone.request();
+      if (microphoneStatus.isPermanentlyDenied) {
+        openAppSettings();
+        return;
+      }
+    }
+
+    print("🆗 ${DateTime.now()}: Dubit - Mic Permission Granted");
+
+    var baseUrl = apiBaseUrl ?? 'https://test-api.dubit.live';
+    var url = Uri.parse('$baseUrl/meeting/new-meeting');
+
+    var headers = {
+      'Authorization': 'Bearer $apiKey',
+      'Content-Type': 'application/json',
+    };
+
+    print("🔄 ${DateTime.now()}: Dubit - Preparing Call & Client...");
+
+    // Create the Dubit call and client creation as futures
+    var dubitCallFuture = http.get(url, headers: headers);
+    var clientCreationFuture =
+        _createClientWithRetries(clientCreationTimeoutDuration);
+
+    // Wait for both futures to complete
+    var results = await Future.wait([dubitCallFuture, clientCreationFuture]);
+
+    var response = results[0] as http.Response;
+    var client = results[1] as CallClient;
+
+    _client = client;
+
+    var webCallUrl = null;
+
+    if (response.statusCode == 200) {
+      print("🆗 ${DateTime.now()}: Dubit - Dubit Call Ready");
+
+      var data = jsonDecode(response.body);
+      webCallUrl = data['roomUrl'];
+      if (webCallUrl == null) {
+        print('🆘 ${DateTime.now()}: Dubit - Dubit Call URL not found');
+        emit(DubitEvent("call-error"));
+        return;
+      }
+    } else {
+      client.dispose();
+      _client = null;
+      print(
+          '🆘 ${DateTime.now()}: Dubit - Failed to create Dubit Call. Error: ${response.body}');
+      emit(DubitEvent("call-error"));
+      return;
+    }
+
+    print("🔄 ${DateTime.now()}: Dubit - Joining Call...");
+
+    client.events.listen((event) {
+      event.whenOrNull(callStateUpdated: (stateData) {
+        switch (stateData.state) {
+          case CallState.leaving:
+          case CallState.left:
+            _client = null;
+            print("⏹️  ${DateTime.now()}: Dubit - Call Ended.");
+
+            emit(DubitEvent("call-end"));
+            break;
+          case CallState.joined:
+            print("🆗 ${DateTime.now()}: Dubit - Joined Call");
+            break;
+          default:
+            break;
+        }
+      }, participantLeft: (participantData) {
+        if (participantData.info.isLocal) return;
+        _client?.leave();
+      }, appMessageReceived: (messageData, id) {
+        _onAppMessage(messageData);
+      }, participantUpdated: (participantData) {
+        if (participantData.info.username == "Dubit Speaker" &&
+            participantData.media?.microphone.state == MediaState.playable) {
+          print("📤 ${DateTime.now()}: Dubit - Sending Ready...");
+          client.sendAppMessage(jsonEncode({'message': "playable"}), null);
+        }
+      }, participantJoined: (participantData) {
+        if (participantData.info.username == "Dubit Speaker" &&
+            participantData.media?.microphone.state == MediaState.playable) {
+          print("📤 ${DateTime.now()}: Dubit - Sending Ready...");
+          client.sendAppMessage(jsonEncode({'message': "playable"}), null);
+        }
+      });
+    });
+
+    client
+        .join(
+            url: Uri.parse(webCallUrl),
+            clientSettings: const ClientSettingsUpdate.set(
+                inputs: InputSettingsUpdate.set(
+              microphone: MicrophoneInputSettingsUpdate.set(
+                  isEnabled: BoolUpdate.set(true)),
+              camera: CameraInputSettingsUpdate.set(
+                  isEnabled: BoolUpdate.set(false)),
+            )))
+        .catchError((e) {
+      throw Exception('🆘 ${DateTime.now()}: Dubit - Failed to join call: $e');
+    });
+  }
+
+  Future<CallClient> _createClientWithRetries(
+    Duration clientCreationTimeoutDuration,
+  ) async {
+    var retries = 0;
+    const maxRetries = 5;
+
+    Future<CallClient> attemptCreation() async {
+      return CallClient.create();
+    }
+
+    Future<CallClient> createWithTimeout() async {
+      var completer = Completer<CallClient>();
+      Future.delayed(clientCreationTimeoutDuration).then((_) {
+        if (!completer.isCompleted) {
+          print("⏳ ${DateTime.now()}: Dubit - Client creation timed out.");
+          completer
+              .completeError(TimeoutException('Client creation timed out'));
+        }
+      });
+
+      attemptCreation().then((client) {
+        if (!completer.isCompleted) {
+          completer.complete(client);
+        }
+      }).catchError((error) {
+        if (!completer.isCompleted) {
+          completer.completeError(error);
+        }
+      });
+
+      return completer.future;
+    }
+
+    while (retries < maxRetries) {
+      try {
+        print(
+            "🔄 ${DateTime.now()}: Dubit - Creating client (Attempt ${retries + 1})...");
+        var client = await createWithTimeout();
+        print("🆗 ${DateTime.now()}: Dubit - Client Created");
+        return client;
+      } catch (e) {
+        retries++;
+        if (retries >= maxRetries) {
+          print(
+              "🆘 ${DateTime.now()}: Dubit - Failed to create client after $maxRetries attempts.");
+          rethrow;
+        }
+      }
+    }
+
+    // This line should theoretically never be reached due to the rethrow above
+    throw Exception('Client creation failed after $maxRetries retries');
+  }
+
+  Future<void> send(dynamic message) async {
+    await _client!.sendAppMessage(jsonEncode(message), null);
+  }
+
+  void _onAppMessage(String msg) {
+    try {
+      var parsedMessage = jsonDecode(msg);
+
+      if (parsedMessage == "listening") {
+        print("✅ ${DateTime.now()}: Dubit - Assistant Connected.");
+        emit(DubitEvent("call-start"));
+      }
+
+      emit(DubitEvent("message", parsedMessage));
+    } catch (parseError) {
+      print("Error parsing message data: $parseError");
+    }
+  }
+
+  Future<void> stop() async {
+    if (_client == null) {
+      throw Exception('No call in progress');
+    }
+    await _client!.leave();
+  }
+
+  void setMuted(bool muted) {
+    _client!.updateInputs(
+        inputs: InputSettingsUpdate.set(
+      microphone:
+          MicrophoneInputSettingsUpdate.set(isEnabled: BoolUpdate.set(!muted)),
+    ));
+  }
+
+  bool isMuted() {
+    return _client!.inputs.microphone.isEnabled == false;
+  }
+
+  @Deprecated(
+    "Use [setDubitAudioDevice] instead. Deprecated because unusable if user does not depend of daily_flutter",
+  )
+
+  /// use [setDubitAudioDevice] instead
+  void setAudioDevice({required DeviceId deviceId}) {
+    _client!.setAudioDevice(deviceId: deviceId);
+  }
+
+  void setDubitAudioDevice({required DubitAudioDevice device}) {
+    _client!.setAudioDevice(
+      deviceId: switch (device) {
+        DubitAudioDevice.speakerphone => DeviceId.speakerPhone,
+        DubitAudioDevice.wired => DeviceId.wired,
+        DubitAudioDevice.earpiece => DeviceId.earpiece,
+        DubitAudioDevice.bluetooth => DeviceId.bluetooth,
+      },
+    );
+  }
+
+  void emit(DubitEvent event) {
+    _streamController.add(event);
+  }
+
+  void dispose() {
+    _streamController.close();
+  }
+}
